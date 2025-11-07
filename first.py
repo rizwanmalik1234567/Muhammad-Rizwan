df615ab297608f70397085864b7e85cc76b33e7c
Malik rizwan <mr7066194@gmail.com>
2025-11-06 23:15:28 -0800

    Add files via upload

---
+++ first.py
@@ -0,0 +1,42 @@
+print("Hello World")
+
+print(3)
+
+print(3,2,1)
+
+print(3+2+1)
+
+print("name ",3+3)
+
+
+A = 22
+B = 33
+C = A + B
+print(C)
+
+age = 22
+name = "Ali"
+print(name,":",age)
+print(age)
+
+A,B,C = "Orange","Banana","Apple"
+print(A,B,C)
+
+Fruit = ["Orange","Banana","Apple"]
+print(Fruit)
+print(type(Fruit))
+
+X=range(10)
+print(X)
+print(type(X))
+
+Student={
+    "Name":"Ali Hassan",
+    "Reg#":"MC-BSCS-243-005",
+    "CGPA":"3.75",
+    "Email":"Ali@example.com",
+    "Phone":"123-456-7890",
+    "Address":"123 Main St, Multan, Pakistan",
+}
+print(Student)
+
